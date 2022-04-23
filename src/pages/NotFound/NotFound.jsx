3295f6d393c4ea71cc6d29e44b7b7c2f3ee54ef8
Raymond Lam <raymond.m.lam@gmail.com>
2022-04-23 11:08:55 +0200

    added content in 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,14 @@
+import notFoundImg from '../../assets/404.gif'
+import './NotFound.css'
+
 export default function NotFound() {
-	return <div>NotFound</div>
+	return (
+		<>
+			<h1 className='text-center text-white'>404: Page not found</h1>
+
+			<div className='not-found d-flex justify-content-center m-4'>
+				<img src={notFoundImg} alt='' />
+			</div>
+		</>
+	)
 }
