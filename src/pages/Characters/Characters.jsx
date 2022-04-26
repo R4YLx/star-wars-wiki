e0b57ae5f13e03345c7e0c483c33ac840423326a
Raymond Lam <raymond.m.lam@gmail.com>
2022-04-26 08:50:57 +0200

    now rendering search results

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -5,12 +5,14 @@ import Loading from '../../components/Loading/Loading'
 import { Link } from 'react-router-dom'
 import SearchBar from '../../components/SearchBar'
 import { useSearchParams } from 'react-router-dom'
+import { getIdFromUrl } from '../../helpers/helpers'
 
 export default function Characters() {
 	const [characters, setCharacters] = useState([])
 	const [loading, setLoading] = useState(false)
 
-	const [searchInput, setSearchInput] = useState('')
+	const [searchInput, setSearchInput] = useState([])
+	const [searchData, setSearchData] = useState([])
 	const [searchResult, setSearchResult] = useState(null)
 	const [page, setPage] = useState(1)
 	const [searchParams, setSearchParams] = useSearchParams()
@@ -34,10 +36,14 @@ export default function Characters() {
 
 		const data = await SwapiAPI.search('people', searchQuery, page)
 
-		setSearchResult(data)
-		setLoading(false)
+		setSearchData(data)
+		setSearchResult(data.results)
 
-		console.log(data)
+		console.log('Data from API:', data)
+		console.log('Hits of characters from API', data.results)
+
+		setLoading(false)
+		setCharacters([])
 	}
 
 	const handleSubmit = async e => {
@@ -50,7 +56,6 @@ export default function Characters() {
 		setPage(1)
 		searchSWAPI(searchInput, 1)
 		setSearchParams({ query: searchInput })
-		console.log(searchResult)
 	}
 
 	useEffect(() => {
@@ -109,25 +114,63 @@ export default function Characters() {
 						</div>
 					))}
 			</div>
-			<div className='d-flex justify-content-between align-items-center p-4'>
-				<button
-					disabled={page === 1}
-					onClick={() => setPage(prevValue => prevValue - 1)}
-					type='button'
-					className='btn btn-secondary'
-				>
-					Previous Page
-				</button>
-				<div className='page'>{page}</div>
-				<button
-					disabled={characters.length < 9}
-					onClick={() => setPage(prevValue => prevValue + 1)}
-					type='button'
-					className='btn btn-secondary'
-				>
-					Next Page
-				</button>
+
+			<div className='d-flex flex-wrap justify-content-center'>
+				{searchResult &&
+					searchResult.map((character, index) => (
+						<div
+							key={index}
+							className='card border-secondary m-3 col-md-3 col-sm-4 col-xs-12'
+						>
+							<div className='card-header d-flex align-items-center'>
+								<h2>{character.name}</h2>
+							</div>
+							<div className='card-body'>
+								<p className='text-dark'>
+									<span>Gender: </span>
+									{character.gender}
+								</p>
+								<hr />
+								<p className='text-dark'>
+									<span>Born: </span>
+									{character.birth_year}
+								</p>
+								<hr />
+								<p className='text-dark'>
+									<span>Appears in: </span>
+									{character.films.length} film(s)
+								</p>
+								<Link to={`/characters/${index + 1}`}>
+									<button type='button' className='btn btn-primary'>
+										Read more
+									</button>
+								</Link>
+							</div>
+						</div>
+					))}
 			</div>
+
+			{characters && (
+				<div className='d-flex justify-content-between align-items-center p-4'>
+					<button
+						disabled={page === 1}
+						onClick={() => setPage(prevValue => prevValue - 1)}
+						type='button'
+						className='btn btn-secondary'
+					>
+						Previous Page
+					</button>
+					<div className='page'>{page}</div>
+					<button
+						disabled={characters.length < 9}
+						onClick={() => setPage(prevValue => prevValue + 1)}
+						type='button'
+						className='btn btn-secondary'
+					>
+						Next Page
+					</button>
+				</div>
+			)}
 		</>
 	)
 }
